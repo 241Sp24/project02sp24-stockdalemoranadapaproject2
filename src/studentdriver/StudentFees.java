0735e5b682f6f7d88a3b9261bfda62c549cdca69
EMoran76 <s564151@nwmissouri.edu>
2024-03-29 10:41:44 -0500

    StudentFees Class
    I added the StudentFees Class, but 2 of them I haven't filled out yet

---
+++ src/studentdriver/StudentFees.java
@@ -0,0 +1,48 @@
+package studentdriver;
+
+public class StudentFees {
+    private String studentName;
+    private int studentID;
+    private boolean isEnrolled;
+    private int CREDITS_PER_COURSE = 3;
+    private double PER_CREDIT_FEE = 543.50;
+    
+    public StudentFees(String studentName, int studentID, boolean isEnrolled){
+        this.studentName = studentName;
+        this.studentID = studentID;
+        this.isEnrolled = isEnrolled;
+    }
+    
+    //Getter Methods
+    public String getStudentName(){
+        return studentName;
+    }
+    public int getStudentID(){
+        return studentID;
+    }
+    public int getCREDITS_PER_COURSE(){
+        return CREDITS_PER_COURSE;
+    }
+    public double getPER_CREDIT_FEE(){
+        return PER_CREDIT_FEE;
+    }
+    
+    //Setter Methods
+    public void setStudentName(String studentName){
+        this.studentName = studentName;
+    }
+    public void setStudentID(int studentID){
+        this.studentID = studentID;
+    }
+    public void setIsEnrolled(boolean isEnrolled){
+        this.isEnrolled = isEnrolled;
+    }
+    
+    public boolean isIsEnrolled(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
