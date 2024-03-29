606190f9eeb1c969c6b5587b3a4f171aabf3b32d
EMoran76 <s564151@nwmissouri.edu>
2024-03-29 14:48:28 -0500

    Class
    Worked on the GraduateStudent class

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -1,1 +1,33 @@
+package studentdriver;
 
+public class GraduateStudent extends StudentFees {
+    private int coursesEnrolled;
+    private boolean isGraduateAssistant;
+    private String graduateAssistantType;
+    private double ADDITIONAL_FEES = 654.45;
+    
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled){
+        super(studentName, studentID, isEnrolled);
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.graduateAssistantType = graduateAssistantType;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+    
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, int coursesEnrolled){
+        super(studentName, studentID, isEnrolled);
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+    
+    //public boolean isIsGraduateAssistant(){
+    
+    //Getter Methods
+    public int getCoursesEnrolled(){
+        return coursesEnrolled;
+    }
+    public double getPayableAmount(){
+        
+    }
+    
+    //public String toString(){
+}
