e1da1a2a4c49acb2202ed9c53e558b73b7354cc9
S562066 <158314135+S562066@users.noreply.github.com>
2024-03-29 10:17:41 -0500

    Update StudentDriver.java

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -1,10 +1,23 @@
 package studentdriver;
 
+
+import java.util.*;
+
 public class StudentDriver {
     public static void main(String[] args) {
 //main code goes here
-    
-    
-    
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter the no of UG students: );   
+String num_of_UG_students = input.nextLine();
+
+System.out.println("Enter the no of Graduate students: );   
+String num_of_grad_students = input.nextLine();
+
+System.out.println("Enter the no of online students: );   
+String num_of_online_students = input.nextLine();
+
+System.out.println("**********Undergraduate students list**********");   
+        
     }
 }
