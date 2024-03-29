6f6c2006c58ef88db02e817f87a2eee9e529860d
S562066 <158314135+S562066@users.noreply.github.com>
2024-03-29 10:27:58 -0500

    Update StudentDriver.java

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -18,6 +18,20 @@ System.out.println("Enter the no of online students: );
 String num_of_online_students = input.nextLine();
 
 System.out.println("**********Undergraduate students list**********");   
-        
+
+//list out undergraduate student infomation
+
+System.out.println("**********Graduate students list**********"); 
+
+//list out graduate student infomation   
+
+System.out.println("**********Online students list**********");   
+
+//list out online student infomation        
+
+System.out.println("**********Undergraduate Students details***********");   
+System.out.println("**********Graduate Students details**********");   
+System.out.println("**********Online Students details**********");   
+    
     }
 }
