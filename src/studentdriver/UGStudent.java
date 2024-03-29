4f21baae776703f6851c41f0b6a516b8df27d7b3
EMoran76 <s564151@nwmissouri.edu>
2024-03-29 14:20:16 -0500

    Class
    Started the class

--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -1,1 +1,15 @@
+package studentdriver;
 
+public class UGStudent extends StudentFees {
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private double ADDITIONAL_FEE = 820.70;
+    
+    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled){
+        super(studentName, studentID, isEnrolled);
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+}
