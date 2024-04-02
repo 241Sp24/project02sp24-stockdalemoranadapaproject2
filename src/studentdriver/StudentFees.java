5c80d1bd27973b68a6c811c68e0099104e6dee96
S562066 <158314135+S562066@users.noreply.github.com>
2024-04-02 18:07:51 -0500

    complete student fees class

--- src/studentdriver/StudentFees.java
+++ src/studentdriver/StudentFees.java
@@ -1,51 +1,58 @@
 package studentdriver;
 
-public abstract class StudentFees {
+abstract class StudentFees {
     private String studentName;
     private int studentID;
     private boolean isEnrolled;
-    private int CREDITS_PER_COURSE = 3;
-    private double PER_CREDIT_FEE = 543.50;
-    
-    public StudentFees(String studentName, int studentID, boolean isEnrolled){
+    final int CREDITS_PER_COURSE = 3;
+    final double PER_CREDIT_FEE = 543.50;
+
+    public StudentFees(String studentName, int studentID, boolean isEnrolled) {
         this.studentName = studentName;
         this.studentID = studentID;
         this.isEnrolled = isEnrolled;
     }
-    
+
     //Getter Methods
-    public String getStudentName(){
+    public String getStudentName() {
         return studentName;
     }
-    public int getStudentID(){
+
+    public int getStudentID() {
         return studentID;
     }
-    //getisEnrolled method
-    public boolean isIsEnrolled(){
+
+    public boolean isIsEnrolled() {
         return isEnrolled;
     }
-    public int getCREDITS_PER_COURSE(){
+
+    public int getCREDITS_PER_COURSE() {
         return CREDITS_PER_COURSE;
     }
-    public double getPER_CREDIT_FEE(){
+
+    public double getPER_CREDIT_FEE() {
         return PER_CREDIT_FEE;
     }
     
     //Setter Methods
-    public void setStudentName(String studentName){
+    public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
-    public void setStudentID(int studentID){
+
+    public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
-    public void setIsEnrolled(boolean isEnrolled){
+
+    public void setIsEnrolled(boolean isEnrolled) {
         this.isEnrolled = isEnrolled;
     }
-    
-    //public boolean isIsEnrolled(){
-    
-    //public String toString(){
-    
-    //Abstract Method
+
     public abstract double getPayableAmount();
+
+    @Override
+    public String toString() {
+        return "Student name: " + studentName + "\n" +
+                "Student id: " + studentID + "\n" +
+                "Enrolled: " + isEnrolled;
+    }
 }
