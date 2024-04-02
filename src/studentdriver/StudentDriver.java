b232b8e6094424f13f81a3e133403862d7475cf1
S562066 <158314135+S562066@users.noreply.github.com>
2024-04-02 18:33:05 -0500

    Update StudentDriver.java

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -1,22 +1,21 @@
 package studentdriver;
 
-
 import java.util.*;
+import java.io.*;
 
 public class StudentDriver {
     public static void main(String[] args) {
-//main code goes here
+
 Scanner input = new Scanner(System.in);
-FileReader fileread = new FileReader("input.csv");
 
 System.out.println("Enter the no of UG students: );   
-String num_of_UG_students = input.nextLine();
+int num_of_UG_students = input.nextLine();
 
 System.out.println("Enter the no of Graduate students: );   
-String num_of_grad_students = input.nextLine();
+int num_of_grad_students = input.nextLine();
 
 System.out.println("Enter the no of online students: );   
-String num_of_online_students = input.nextLine();
+int num_of_online_students = input.nextLine();
 
 System.out.println("**********Undergraduate students list**********");   
 
