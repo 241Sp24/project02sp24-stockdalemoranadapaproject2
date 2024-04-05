ec7c878b8a81d0fd980f5e443069fd9d3b0c0e36
S562066 <158314135+S562066@users.noreply.github.com>
2024-04-05 10:09:03 -0500

    Update StudentDriver.java

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class StudentDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 Scanner input = new Scanner(System.in);
 
