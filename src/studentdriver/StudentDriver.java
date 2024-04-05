5b9a2e50da5de10e4920146bbb6ca08432585482
EMoran76 <s564151@nwmissouri.edu>
2024-04-05 10:22:52 -0500

    Structure
    I shifted things over to the right

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -6,51 +6,41 @@ import java.io.*;
 public class StudentDriver {
     public static void main(String[] args) throws Exception {
 
-Scanner input = new Scanner(System.in);
-Scanner fileInput = new Scanner(myFile);
+    Scanner input = new Scanner(System.in);
+    Scanner fileInput = new Scanner(myFile);
 
-System.out.println("Enter the no of UG students: ");   
-int num_of_UG_students = input.nextLine();
+    System.out.println("Enter the no of UG students: ");   
+    int num_of_UG_students = input.nextLine();
 
-System.out.println("Enter the no of Graduate students: ");   
-int num_of_grad_students = input.nextLine();
+    System.out.println("Enter the no of Graduate students: ");   
+    int num_of_grad_students = input.nextLine();
 
-System.out.println("Enter the no of online students: ");   
-int num_of_online_students = input.nextLine();
+    System.out.println("Enter the no of online students: ");   
+    int num_of_online_students = input.nextLine();
 
-StudentFees[] students = new StudentFees[12]; //create array for everything
+    StudentFees[] students = new StudentFees[12]; //create array for everything
 
-/*
-File source_file = new File("input.csv");
-while(fileInput.hasNext())
-{
-put everything in the file in an array
-}
-fileInput.close();
-*/
-
-System.out.println("**********Undergraduate students list**********");   
-
-//list out undergraduate student infomation
-
-System.out.println("**********Graduate students list**********"); 
-
-//list out graduate student infomation   
-
-System.out.println("**********Online students list**********");   
-
-//list out online student infomation        
-for(StudentFees s: stuList){
-    if(s instanceof UGStudent){
-        System.out.println("**********Undergraduate Students list***********"); 
+    /*
+    File source_file = new File("input.csv");
+    while(fileInput.hasNext())
+    {
+    put everything in the file in an array
     }
-    else if(s instanceof GraduateStudent){
-        System.out.println("**********Graduate Students list**********");  
+    fileInput.close();
+    */  
+
+    //Loop to print out all student details       
+    for(StudentFees s: stuList){
+        if(s instanceof UGStudent){
+            System.out.println("**********Undergraduate Students list***********"); 
+        }
+        else if(s instanceof GraduateStudent){
+            System.out.println("**********Graduate Students list**********");  
+        }
+        else{
+            System.out.println("**********Online Students list**********");  
+        }
     }
-    else{
-        System.out.println("**********Online Students list**********");  
-    }
-}      
-    
+
     }
 }
